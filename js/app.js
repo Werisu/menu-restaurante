ed6ffdae30ebbd23076124eafdd5b8b82b26eb36
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-05 11:11:24 -0300

    test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./service-worker.js")
+      .register(
+        "https://werisu.github.io/menu-restaurante/js/service-worker.js"
+      )
       .then((registration) => {
         console.log("Service Worker registrado com sucesso:", registration);
       })
