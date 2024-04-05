409a2ab4c7a73935481cee8d0a11b6f9a2bc185b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-05 11:03:32 -0300

    test

--- js/app.js
+++ js/app.js
@@ -1,70 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register(() => {
-        const CACHE_NAME = "menu-online-cache-v1";
-        const urlsToCache = [
-          "/",
-          "/index.html",
-          "/css/main.css",
-          "/js/app.js",
-          "/img/logo.png",
-        ];
-
-        self.addEventListener("install", (event) => {
-          event.waitUntil(
-            caches.open(CACHE_NAME).then((cache) => {
-              return cache.addAll(urlsToCache);
-            })
-          );
-        });
-
-        self.addEventListener("fetch", (event) => {
-          event.respondWith(
-            caches.match(event.request).then((response) => {
-              if (response) {
-                return response;
-              }
-
-              const fetchRequest = event.request.clone();
-
-              return fetch(fetchRequest).then((response) => {
-                if (
-                  !response ||
-                  response.status !== 200 ||
-                  response.type !== "basic"
-                ) {
-                  return response;
-                }
-
-                const responseToCache = response.clone();
-
-                caches.open(CACHE_NAME).then((cache) => {
-                  cache.put(event.request, responseToCache);
-                });
-
-                return response;
-              });
-            })
-          );
-        });
-
-        self.addEventListener("activate", (event) => {
-          event.waitUntil(
-            caches.keys().then((cacheNames) => {
-              return Promise.all(
-                cacheNames
-                  .filter((cacheName) => {
-                    return cacheName !== CACHE_NAME;
-                  })
-                  .map((cacheName) => {
-                    return caches.delete(cacheName);
-                  })
-              );
-            })
-          );
-        });
-      })
+      .register("/js/service-worker.js")
       .then((registration) => {
         console.log("Service Worker registrado com sucesso:", registration);
       })
