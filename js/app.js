1cdb7a444d36bcef405f5beb26a7abcf99cf18e0
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-05 10:58:11 -0300

    test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("/js/service-worker.js")
       .then((registration) => {
         console.log("Service Worker registrado com sucesso:", registration);
       })
