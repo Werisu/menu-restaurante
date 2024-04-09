bca79c61eca9d7d29aff56a917e37e0551118eaf
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-09 09:08:18 -0300

    update service worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -22,19 +22,3 @@ self.addEventListener("fetch", (fetchEvent) => {
     })
   );
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => {
-            return cacheName !== CACHE_NAME;
-          })
-          .map((cacheName) => {
-            return caches.delete(cacheName);
-          })
-      );
-    })
-  );
-});
